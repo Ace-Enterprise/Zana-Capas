7f474657a34558e1e8c537b8890d9a97c3bd4732
Miguel Wiest <wiestmiguel@gmail.com>
2025-02-18 14:16:45 -0300

    refact: images

--- views/Vehicles/index.js
+++ views/Vehicles/index.js
@@ -1,22 +1,28 @@
 import styles from "./styles.module.scss";
 
+import Image from "next/image";
+
+import carroImg from "../../assets/carro.png";
+import motoImg from "../../assets/moto.png";
+import bikeImg from "../../assets/bike.png";
+
 export const Vehicles = () => {
   const vehicles = [
     {
       title: "Carro",
-      image: "",
+      image: carroImg,
       description:
         "Com opções de forro total, parcial ou sem forro. <br/> <br/> Tamanhos: P, M, G, GG, XG, XGG, VAN, e KOMBI. <br/> <br/> Modelos versáteis para proteção diária, tanto em ambientes internos quanto externos.",
     },
     {
       title: "Moto",
-      image: "",
+      image: motoImg,
       description:
         "Capas forradas e sem forro, projetadas para oferecer o máximo de proteção e praticidade. <br/> <br/> Tamanhos:  P, M, G, GG, XG, XGG <br/> <br/> Disponíveis em tamanho ajustável, ideais para diferentes tipos de motocicletas.",
     },
     {
       title: "Bike",
-      image: "",
+      image: bikeImg,
       description:
         "Com e sem forro, além de compacta para fácil transporte e uso diário. <br/> <br/> Tamanho único",
     },
@@ -28,7 +34,7 @@ export const Vehicles = () => {
         <div className={styles.content} key={index}>
           <div className={styles.presentation}>
             <h1>{vehicle.title}</h1>
-            <span></span>
+            <Image src={vehicle.image} alt={vehicle.title} width={400} height={400}/>
           </div>
           <div className={styles.text}>
             <p
